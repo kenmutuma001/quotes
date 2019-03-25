0865113494d7732c070678c016f95bf6ca05e013
kennedymutuma <santa@northpole.com>
2019-03-25 10:48:18 +0300

    create the thumbsup and thumbsdown button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateCountPipe } from './date-count.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesFormComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    DateCountPipe,
+  
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
